06766e732eab92b5172da6ca7f1ac176f157255e
udondan <deemes79@gmail.com>
2014-10-12 12:02:51 +0200

    Upgraded js-yaml to 3.2.2

--- package.js
+++ package.js
@@ -1,14 +1,17 @@
 var where = ["server"];
 Package.describe({
   summary: "A YAML 1.2 parser and serializer. Converts YAML files to JSON and JS objects to YAML.",
+  version: "3.2.2_1",
+  git: "https://github.com/udondan/meteor-yml.git",
   environments: where
 });
 
 Npm.depends({
-  'js-yaml': '3.1.0'
+  'js-yaml': '3.2.2'
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom("METEOR@0.9.0");
   api.add_files('lib/yml.js', where);
   api.export(['YAML'], where);
 });
